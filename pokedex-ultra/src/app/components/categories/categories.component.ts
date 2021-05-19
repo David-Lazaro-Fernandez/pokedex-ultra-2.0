c06f83f12a1ffa1da9a49e6b77b6a0c68b98fdab
David Lazaro Fernandez <davidlazaro20@hotmail.com>
2021-05-19 11:42:18 -0500

    Saved changes

--- pokedex-ultra/src/app/components/categories/categories.component.ts
+++ pokedex-ultra/src/app/components/categories/categories.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-categories',
+  selector: 'Categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent implements OnInit {
+export class CategoriesComponent {
 
-  constructor() { }
+  @Input() colores = Colores.amarillo
+  @Input() colorSecundario = Colores.amarillo
 
-  ngOnInit() {}
+  secondaryCardColor = {
+    [Colores.rojo] : "#f98b7e",
+    [Colores.azul] : "#71bffc",
+    [Colores.amarillo] : "#ffda5b",
+    [Colores.verde] : "#57d0b1",
+    [Colores.naranja] : "#f5a771",
+    [Colores.morado] : "#9569a5"
+  }
+  cardColors = {
+    [Colores.rojo] : "#f7786b",
+    [Colores.azul] : "#58aaf6",
+    [Colores.amarillo] : "#ffce4b",
+    [Colores.verde] : "#4fc1a6",
+    [Colores.naranja] : "#F08A42",
+    [Colores.morado] : "#7c538c"
+  }
+  cardShadow = {
+    [Colores.rojo] : "RGBA(247, 120, 107,0.5)",
+    [Colores.azul] : "RGBA(88, 170, 246,0.5)",
+    [Colores.amarillo] : "RGBA(255, 206, 75,0.5)",
+    [Colores.verde] : "RGBA(79, 193, 166,0.5)",
+    [Colores.naranja] : "RGBA(240, 138, 66,0.5)",
+    [Colores.morado] : "RGBA(124, 83, 140,0.5)"
+  }
 
+}
+
+export enum Colores{
+  rojo,
+  amarillo,
+  verde,
+  azul,
+  naranja,
+  morado
 }
--- pokedex-ultra/src/app/components/poke-news/poke-news.service.ts
+++ pokedex-ultra/src/app/components/poke-news/poke-news.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeNewsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+     
+   }
+
+  getPokemones(palabra = "Pokemon", API_KEY="cfb3ebdf70d64840959ff3b58c69e6f0"){
+    return  this.http.get<any>(`https://newsapi.org/v2/everything?q=${palabra}&from=2021-05-16&sortBy=popularity&apiKey=${API_KEY}`)
+  }
 }
