d74763127281b5b4b9869399de337bb11d0fe24d
LaloCode <lalo_pichu22@hotmail.com>
2021-05-26 20:44:51 -0500

    trainer card progress

--- pokedex-ultra/src/app/trainer-card/trainer-card.page.ts
+++ pokedex-ultra/src/app/trainer-card/trainer-card.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-trainer-card',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerCardPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  ruta(){
+    console.log("Test")
+    this.router.navigate(['/home'])
+  }
 }
--- pokedex-ultra/src/assets/seed.sql
+++ pokedex-ultra/src/assets/seed.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS user(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     uid TEXT, 
-    photo_route TEXT
+    gender TEXT
 );
 
 CREATE TABLE IF NOT EXISTS favorite_pkmn(
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS favorite_pkmn(
     FOREIGN KEY(user) REFERENCES user(id)
 );
 
-INSERT or IGNORE INTO user(id, uid, photo_route) VALUES (1, 'xd', 'assets/userImgs/default.jpg');
+INSERT or IGNORE INTO user(id, uid, gender) VALUES (1, 'xd', 'm');
 INSERT or IGNORE INTO favorite_pkmn(id, pkmn_id, user) VALUES (1, 1, 1);
